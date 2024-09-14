e8dac01f92c11c4691fbf9a86edd5b668c5b030f
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-09-13 23:36:43 -0300

    Implementação do Controller e Model da Tabela Role

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/roles', [RoleController::class, 'create']);
+Route::put('/roles/{role}', [RoleController::class, 'update']);
+Route::get('/roles/{role}/users', [RoleController::class, 'getUsers']);
