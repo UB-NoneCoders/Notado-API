db3ff9a5aea7ed454776f6ee22525d8951ca4395
Guilherme <guilherme.felipe.noleto@gmail.com>
2024-09-15 17:55:35 -0300

    Update Crud

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource('Subject', SubjectController::class);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
