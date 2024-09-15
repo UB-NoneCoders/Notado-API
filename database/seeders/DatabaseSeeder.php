7e60ce1787e729aa2979780c2837e03061a9069f
luanklo <luanj.kloh@hotmail.com>
2024-09-15 18:47:29 -0300

    mudança no seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RoleSeeder::class,
+            roleSeeder::class,
             userSeeder::class,
             SubjectSeeder::class,
             TestSeeder::class,
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class RoleSeeder extends Seeder
+class roleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
